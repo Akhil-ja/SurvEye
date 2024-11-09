08fd5686bfc177f2243e55ed384a53046e6fcd6c
Akhil Jagadish <akhiljagadish124@gmail.com>
2024-11-09 22:23:09 +0530

    chore:removed logs from auth

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -1,9 +1,10 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
-import { Request, Response, NextFunction } from "express";
-import User from "../models/usersModel";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import User from '../models/usersModel';
+import { IUser } from '../interfaces/common.interface';
 
 interface CustomRequest extends Request {
-  user?: any;
+  user?: IUser;
 }
 
 const protect = async (
@@ -12,35 +13,32 @@ const protect = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    console.log("in protect");
-
     const token = req.cookies?.user;
 
     if (!token) {
-      res.status(401).json({ message: "Not authorized, no token" });
+      res.status(401).json({ message: 'Not authorized, no token' });
       return;
     }
 
     if (!process.env.JWT_SECRET) {
-      throw new Error("JWT_SECRET is not defined");
+      throw new Error('JWT_SECRET is not defined');
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
-    const user = await User.findById(decoded.id).select("-password");
+    const user = await User.findById(decoded.id).select('-password');
 
     if (!user) {
-      res.status(401).json({ message: "User not found" });
+      res.status(401).json({ message: 'User not found' });
       return;
     }
-    console.log(user);
 
     req.user = user;
     next();
   } catch (err) {
     if (err instanceof jwt.JsonWebTokenError) {
-      res.status(401).json({ message: "Invalid token....." });
+      res.status(401).json({ message: 'Invalid token.....' });
     } else {
-      res.status(500).json({ message: "Server error" });
+      res.status(500).json({ message: 'Server error' });
     }
   }
 };
